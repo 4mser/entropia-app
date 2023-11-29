c041fdf4886530caeac6328618e2de29ae01f724
4mser <nicolas.moreno.sagan@gmail.com>
2023-11-29 11:59:17 -0300

    perfil arreglado

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { bottombarLinks } from "@/constants";
+import { useAuth } from "@clerk/nextjs";
 
 function Bottombar() {
   const pathname = usePathname();
+  const { userId } = useAuth();
+
 
   return (
     <section className='bottombar'>
@@ -15,6 +18,8 @@ function Bottombar() {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+            if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
           return (
             <Link
