d8c5dd2411d78fc297c717a26cfe2faa51122d54
4mser <nicolas.moreno.sagan@gmail.com>
2023-11-29 18:48:35 -0300

    boton landpage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,6 +6,7 @@ import { dark } from "@clerk/themes";
 import { esES } from '@clerk/localizations'
 
 import "../globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,8 +34,13 @@ export default function RootLayout({
         <meta property="og:image" content="https://app-valdi.s3.amazonaws.com/instagram-12.png" />
       </head>
         <body className={`${inter.className}  w-full  bg-dark-1 flex flex-col  items-center `}>
-          <header className="flex justify-start w-full p-5 mb-14">
-          <img src="/entropia.svg" alt="logo" className="w-32" />
+          <header className="flex justify-between items-center w-full p-5 mb-8 md:px-[10%] xl:px-[15%]">
+            <img src="/entropia.svg" alt="logo" className="w-28" />
+            <Link href="https://demoentropia.vercel.app/" className="bg-gradient-to-tr group from-blue to-green-700 h-9 rounded-full flex items-center justify-center p-px">
+              <div className="bg-dark-1 rounded-full group-hover:bg-opacity-60 h-full w-full flex justify-center items-center text-center text-white px-3 gap-1">
+                Ver landpage <span className="group-hover:translate-x-1 transition">{'->'}</span>
+              </div>
+            </Link>
           </header>
           {children}
         </body>
