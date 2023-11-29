c7fb28259fda397373620856b191baa28a95d504
4mser <nicolas.moreno.sagan@gmail.com>
2023-11-29 08:55:46 -0300

    first commit

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,41 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Link from "next/link";
+
+function Topbar() {
+  return (
+    <nav className='topbar'>
+      <Link href='/' className='flex items-center gap-4'>
+        <img src='/entropia.svg' alt='logo' width={110} height={110} />
+      </Link>
+
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
+          <SignedIn>
+            <SignOutButton>
+              <div className='flex cursor-pointer'>
+                <img
+                  src='/assets/logout.svg'
+                  alt='logout'
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Topbar;
