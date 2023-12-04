cf716488622a87321f5b3dbce11625e902e47f55
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-04 20:42:41 -0300

    rlab update2

--- app/(root)/research-lab/page.tsx
+++ app/(root)/research-lab/page.tsx
@@ -4,10 +4,11 @@ function page() {
     <div>
         <img src="/images/1.png" alt="entropia-research-lab" className="  overflow-hidden border-b border-white/10 py-2" />
 
-        <div className="p-3 text-white text-custom-body">
-          <h2 className="text-base-medium">Ciencia, tecnología e innovación para impulsar un futuro sostenible.</h2>
-          <p className="opacity-80 pt-1">Este laboratorio representa el núcleo vital de nuestra investigación, desempeñando un papel crucial en disciplinas que abarcan desde física, inteligencia artificial y deep learning hasta automatización, robótica y la preservación de los recursos naturales.</p>
+        <div className="p-3 text-white text-custom-body border-b border-white/10 bg-dark-2">
+          <h2 className="text-base-medium">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
+          <p className="opacity-80 pt-1">Este laboratorio representa el núcleo vital de nuestra investigación, desempeñando un papel crucial en disciplinas que abarcan desde física, inteligencia artificial y deep learning hasta automatización, robótica y la preservación de recursos naturales.</p>
         </div>
+
 
 
     </div>
