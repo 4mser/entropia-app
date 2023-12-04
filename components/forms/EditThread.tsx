b463480843d9a95b00499035a602769e8e95a7b6
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-04 15:08:29 -0300

    bug arreglado para editar posts

--- components/forms/EditThread.tsx
+++ components/forms/EditThread.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { editThread } from "@/lib/actions/thread.actions";
 import Loader from "../ui/loader";
+import { Textarea } from "../ui/textarea";
 
 interface Props {
   threadId: string;
@@ -55,10 +56,11 @@ function EditThread({
           {isEditing ? (
             <div className="w-full h-screen bg-dark-1/90 fixed z-50 top-0 left-0 flex justify-center items-center">
               <div className="bg-dark-1 rounded-xl border border-white/10 overflow-hidden p-5 flex flex-col">
-                <textarea
+                <Textarea
+                rows={5}
                   value={newText}
                   onChange={(e) => setNewText(e.target.value)}
-                  className="bg-dark-2 rounded-md p-2 text-white"
+                  className="bg-dark-2 rounded-md p-2 text-white h-auto w-auto"
                 />
                 <div className="flex flex-col gap-2 mt-3">
                 <button onClick={handleEdit} className="text-white rounded-lg bg-gradient-to-tr from-blue to-green-700 py-1">
