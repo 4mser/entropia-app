d4ab6c46ec53dd93dd068ea855191f3c9db0fba7
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-04 20:27:38 -0300

    loader ajustado

--- app/(root)/search/loading.tsx
+++ app/(root)/search/loading.tsx
@@ -2,7 +2,7 @@ import '../../globals.css'
 
 const Loading = () => {
   return (
-        <section className='w-full h-96  flex items-center justify-center'>
+        <section className='w-full h-[85vh]  flex items-center justify-center'>
             <div className='particle-container'>
             {[...Array(13)].map((_, index) => (
                 <div className="particle" key={index}></div>
