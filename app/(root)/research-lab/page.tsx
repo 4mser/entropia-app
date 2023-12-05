31ce947598ca9a48750e08150558b4611e3aefc6
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-05 00:22:26 -0300

    rlabv3

--- app/(root)/research-lab/page.tsx
+++ app/(root)/research-lab/page.tsx
@@ -3,10 +3,12 @@ import Projects from "@/components/forms/Projects"
 function page() {
   return (
     <div className=" text-white">
-        <img src="/images/1.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 pr-5" />
+        <div className="w-full border-b border-white/10">
+          <img src="/images/1.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-72" />
+        </div>
 
-          <h2 className="p-3 bg-dark-2 text-base-medium border-b border-t border-white/10">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
-          <p className="p-3 opacity-80 pt-2 text-custom-body">Este laboratorio representa el núcleo vital de nuestra investigación, desempeñando un papel crucial en disciplinas que abarcan desde física, inteligencia artificial y deep learning hasta automatización, robótica y la preservación de recursos naturales.</p>
+          <h2 className="px-3 pt-3 pb-1 text-base-medium">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
+          <p className="px-3 pb-3 opacity-80  text-custom-body border-b border-white/10">E.R.LAB abarca campos desde la física, inteligencia artificial y deep learning hasta la automatización, robótica y la preservación de recursos naturales.</p>
         <section>
           <h1 className="p-3 text-white">Proyectos</h1>
           <Projects />
