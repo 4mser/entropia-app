7a204a6d18e8a313931ece251fb1a575b214f891
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-05 16:02:53 -0300

    cambio logo rlab

--- app/(root)/research-lab/page.tsx
+++ app/(root)/research-lab/page.tsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <div className=" text-white">
         <div className="w-full border-b border-white/10">
-          <img src="/images/rlab.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-72" />
+          <img src="/images/1.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-72" />
         </div>
 
           <h2 className="bg-dark-2 px-3 py-2 text-[14px] border-b border-white/10">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
