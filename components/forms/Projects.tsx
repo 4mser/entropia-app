7b01c7e9571d8990d7ed12b9b1bfed3045e1b64f
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-06 20:06:52 -0300

    rlab

--- components/forms/Projects.tsx
+++ components/forms/Projects.tsx
@@ -18,23 +18,15 @@ function Projects() {
       name: 'M.A.P.S',
       image: '/images/OIP.jpeg',
     },
-    {
-      name: 'Deep Eye',
-      image: '/images/img-eye.jpg',
-    },
-    {
-      name: 'M.A.P.S',
-      image: '/images/OIP.jpeg',
-    },
   ];
 
   return (
-    <section className=' overflow-hidden'>
-      <Swiper slidesPerView={2.2} spaceBetween={10} className='mySwiper'>
+    <section className=' overflow-hidden '>
+      <Swiper slidesPerView={2.2} spaceBetween={0} className='mySwiper '>
         {projects.map((project, index) => (
-          <SwiperSlide key={index} className='overflow-hidden'>
-            <div className={`border border-white/10 rounded-md  shadow-md overflow-hidden ${index === 0 ? 'ml-3' : ''}`}>
-              <img src={project.image} alt={project.name} className='w-full h-20 object-cover border-b border-white/10 ' />
+          <SwiperSlide key={index} className='overflow-hidden '>
+            <div className={`border border-white/10 rounded-md  shadow-md overflow-hidden ml-3`}>
+              {/* <img src={project.image} alt={project.name} className='w-full h-20 object-cover border-b border-white/10 ' /> */}
               <p className='text-[12px] p-1 text-white'>{project.name}</p>
             </div>
           </SwiperSlide>
