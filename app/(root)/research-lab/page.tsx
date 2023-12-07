c499fb9359b0125a71cc3e94ff26a1c4a5fb0bee
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-06 21:58:36 -0300

    rlab

--- app/(root)/research-lab/page.tsx
+++ app/(root)/research-lab/page.tsx
@@ -3,8 +3,8 @@ import Projects from "@/components/forms/Projects"
 function page() {
   return (
     <div className="">
-        <div className="w-full border-b border-white/10 shadow-custom shadow-slate-800" >
-          <img src="/images/1.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-72 xl:w-96 " />
+        <div className="w-full border-b border-white/10 shadow-custom shadow-indigo-800/30" >
+          <img src="/images/logorlab.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-72 xl:w-96 " />
         </div>
 
           <h2 className=" px-3 py-2 text-[14px] border-b border-white/10 text-white font-light ">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
