745a6cbcf2d74dc24e15aa5f4119a895077b47a6
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-06 22:09:45 -0300

    rlab update

--- app/(root)/research-lab/page.tsx
+++ app/(root)/research-lab/page.tsx
@@ -3,11 +3,11 @@ import Projects from "@/components/forms/Projects"
 function page() {
   return (
     <div className="">
-        <div className="w-full border-b border-white/10 shadow-custom shadow-indigo-800/30" >
-          <img src="/images/logorlab.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-72 xl:w-96 " />
+        <div className="w-full px-4 py-1 border-b border-white/10 " >
+          <img src="/images/rlabnoicon.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-64 xl:w-96 " />
         </div>
 
-          <h2 className=" px-3 py-2 text-[14px] border-b border-white/10 text-white font-light ">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
+          <h2 className=" px-4 py-2 text-[14px]  border-white/10 text-white font-light ">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
           {/* <p className="p-3 opacity-80  text-[11px]">E.R.LAB abarca campos desde la física, inteligencia artificial y deep learning hasta la automatización, robótica y la preservación de recursos naturales.</p> */}
         {/* <section>
           <h1 className="px-3 pb-2 mt-3 text-white">Proyectos</h1>
