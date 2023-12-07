e1bdc9c5407fecb93db8e7cf1816972cefdd7d7a
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-07 00:03:09 -0300

    mapbox update

--- components/shared/Maps.tsx
+++ components/shared/Maps.tsx
@@ -1,17 +1,18 @@
 'use client'
-import React, { useEffect, useRef, useState } from "react";
-import mapboxgl from "mapbox-gl";
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import mapboxgl, { LngLatLike, Marker } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 function Maps() {
   const [map, setMap] = useState<mapboxgl.Map | null>(null);
   const mapNode = useRef<HTMLDivElement | null>(null);
+  const [userLocation, setUserLocation] = useState<LngLatLike | null>(null);
+  const markerRef = useRef<Marker | null>(null);
 
   useEffect(() => {
     const node = mapNode.current;
     if (typeof window === "undefined" || node === null) return;
 
-    // Get user's location using the Geolocation API
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
@@ -25,9 +26,12 @@ function Maps() {
         });
 
         setMap(mapboxMap);
+        setUserLocation([longitude, latitude]);
 
         return () => {
-          mapboxMap.remove();
+          if (mapboxMap) {
+            mapboxMap.remove();
+          }
         };
       },
       (error) => {
@@ -35,6 +39,37 @@ function Maps() {
       }
     );
   }, []);
+
+  const createMarkerElement = useCallback(() => {
+    const markerElement = document.createElement("div");
+    markerElement.className = "custom-marker"; // Apply Tailwind classes here
+
+    markerElement.style.width = "20px";
+    markerElement.style.height = "20px";
+    markerElement.style.borderRadius = "50%";
+    markerElement.style.backgroundColor = "#4CAF50"; // Green color
+
+    return markerElement;
+  }, []);
+
+  useEffect(() => {
+    if (map && userLocation) {
+      // Remove previous marker if exists
+      if (markerRef.current) {
+        markerRef.current.remove();
+      }
+
+      // Create a new marker with the updated user location
+      const marker = new mapboxgl.Marker({
+        element: createMarkerElement(),
+      })
+        .setLngLat(userLocation)
+        .addTo(map);
+
+      // Save the marker reference for cleanup
+      markerRef.current = marker;
+    }
+  }, [map, userLocation, createMarkerElement]);
 
   return <div ref={mapNode} style={{ width: "100%", height: "100vh" }} />;
 }
