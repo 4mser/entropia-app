ce05e4668ac1f83f998abf277b3b56da0a1a00ff
4mser <nicolas.moreno.sagan@gmail.com>
2023-12-16 17:03:29 -0300

    boton caminata mapa

--- app/(root)/map/page.tsx
+++ app/(root)/map/page.tsx
@@ -4,8 +4,8 @@ function page() {
   return (
     <section className="w-full h-full overflow-hidden">
         <Maps />
-        <div className="fixed bottom-16 right-4 rounded-full flex flex-col p-2 justify-center items-center w-14 h-14 overflow-hidden bg-gradient-to-tr from-blue/50 to-green-500/50 backdrop-blur-sm ">
-          <img src="./assets/walk.svg" alt="" className="translate-x-px" />
+        <div className="fixed bottom-16 right-4 rounded-full flex flex-col p-2 justify-center items-center w-14 h-14 overflow-hidden bg-black/20 border-2 border-white/70 backdrop-blur-sm shadow-custom shadow-white/10">
+          <img src="./assets/walk.svg" alt="" className="translate-x-px opacity-80" />
           <p className="font-medium text-[10px] text-white">0</p>
         </div>
     </section>
