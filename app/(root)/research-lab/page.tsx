6018bc9328831e6f14371cd4a7b2c3fffe756304
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-01 22:26:58 -0300

    research lab logo

--- app/(root)/research-lab/page.tsx
+++ app/(root)/research-lab/page.tsx
@@ -7,13 +7,6 @@ function page() {
         </div>
 
           <h2 className=" px-4 py-2 text-[14px]  border-white/10 text-white font-light ">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
-          {/* <p className="p-3 opacity-80  text-[11px]">E.R.LAB abarca campos desde la física, inteligencia artificial y deep learning hasta la automatización, robótica y la preservación de recursos naturales.</p> */}
-        {/* <section>
-          <h1 className="px-3 pb-2 mt-3 text-white">Proyectos</h1>
-          <Projects />
-        </section> */}
-
-        
 
     </div>
   )
