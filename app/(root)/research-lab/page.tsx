29fb5e9eecf988955bc498fb4ade2a85a7135ebe
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-01 22:20:33 -0300

    logo rlab

--- app/(root)/research-lab/page.tsx
+++ app/(root)/research-lab/page.tsx
@@ -1,10 +1,9 @@
-import Projects from "@/components/forms/Projects"
 
 function page() {
   return (
     <div className="">
         <div className="w-full px-4 py-1 border-b border-white/10 " >
-          <img src="/images/rlabnoicon.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-64 xl:w-96 " />
+          <img src="/images/1.png" alt="entropia-research-lab" className="  overflow-hidden  py-2 w-72 xl:w-96 -translate-x-3" />
         </div>
 
           <h2 className=" px-4 py-2 text-[14px]  border-white/10 text-white font-light ">Ciencia, tecnología e innovación para impulsar un futuro sostenible</h2>
@@ -15,7 +14,6 @@ function page() {
         </section> */}
 
         
-
 
     </div>
   )
