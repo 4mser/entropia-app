5f66ac290d4cbb4451b9f04fe1373c2117c0369e
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-14 20:34:28 -0300

    test 4 contapasos

--- components/shared/ContadorPasos.tsx
+++ components/shared/ContadorPasos.tsx
@@ -16,8 +16,9 @@ function ContadorPasos() {
   const dragConstraints = { top: 0, bottom: 0 }; // Restringe el arrastre verticalmente
 
   return (
-      <div
-        className="fixed md:absolute top-4 left-1/2 -translate-x-1/2  flex rounded-3xl  p-2 justify-between flex-row items-center w-fit gap-3 h-12 overflow-hidden bg-black/20 backdrop-blur-sm border border-white/10 text-white/70 text-[14px] px-3"
+      <section className='fixed top-4 left-0 w-full h-fit flex justify-center'>
+        <div
+        className=" flex rounded-3xl  p-2 justify-between flex-row items-center w-fit gap-3 h-12 overflow-hidden bg-black/20 backdrop-blur-sm border border-white/10 text-white/70 text-[14px] px-3"
         onClick={toggleMenu}
       >
           <img src='../assets/walk.svg' alt="logo" className="w-full h-full object-contain rounded-full shadow-2xl cursor-pointer" />
@@ -26,6 +27,7 @@ function ContadorPasos() {
           <img src='../assets/cripto.svg' alt="logo" className="w-full h-full object-contain rounded-full shadow-2xl cursor-pointer" />
           <p>1200</p>
       </div>
+      </section>
 
   );
 }
