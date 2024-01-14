2abc2fe6728ecfe0c7b3735e0876b91cdae57b43
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-14 20:57:09 -0300

    por favor

--- components/shared/ContadorPasos.tsx
+++ components/shared/ContadorPasos.tsx
@@ -1,6 +1,3 @@
-'use client'
-
-
 
 
 function ContadorPasos() {
@@ -10,15 +7,11 @@ function ContadorPasos() {
         <div
         className="flex rounded-3xl  py-2 px-3 justify-between w-auto items-center  gap-3 h-12 bg-black/20 backdrop-blur-sm border border-white/10 text-white/70 text-[14px] "
         >
-          <div className='w-full h-full flex items-center gap-2'>
-            <img src='../assets/walk.svg' alt="logo" className="w-full h-full object-contain rounded-full shadow-2xl cursor-pointer" />
-            <p>240</p>
-          </div>
+            <img src='../assets/walk.svg' alt="logo" className="h-full object-contain " />
+            <p>24</p>
 
-          <div className='w-full h-full flex items-center gap-2'>
-            <img src='../assets/cripto.svg' alt="logo" className="w-full h-full object-contain rounded-full shadow-2xl cursor-pointer" />
+            <img src='../assets/cripto.svg' alt="logo" className="  h-full object-contain " />
             <p>12</p>
-          </div>
         </div>
       </section>
 
