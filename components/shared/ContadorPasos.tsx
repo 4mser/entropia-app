48b08b5b880c1856466b74f9d11fd9b60b929cf6
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-14 21:55:37 -0300

    no funcionó la animacion culia fea

--- components/shared/ContadorPasos.tsx
+++ components/shared/ContadorPasos.tsx
@@ -16,7 +16,7 @@ function ContadorPasos() {
   return (
       <section className='fixed top-4 left-0 w-full h-fit flex justify-center select-none '>
         <div
-        className="flex flex-col rounded-3xl  py-2 px-3 justify-between w-auto items-center  gap-3 h-auto bg-black/20 backdrop-blur-sm border border-white/10 text-white/70 text-[14px] " 
+        className="flex flex-col rounded-3xl  py-2 px-3 justify-between w-auto items-center  gap-3  bg-black/20 backdrop-blur-sm border border-white/10 text-white/70 text-[14px] " 
         onClick={toggleMenu}
         >
             <div className='flex items-center h-7 gap-3 '>
@@ -34,7 +34,7 @@ function ContadorPasos() {
                   initial={{ height: '0' }}
                   animate={{ height: 'auto' }}
                   exit={{ height: 0 }}
-                  transition={{ ease: 'backInOut', duration: 0.2 }}
+                  transition={{ ease: 'backInOut', duration: 0.5 }}
                 >
                   <img src="../images/grafico.png" alt="" />
                 </motion.div>
