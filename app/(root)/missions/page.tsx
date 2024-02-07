9869bad8b65a5b60bfa9beb6adcf959bd35feb6f
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-07 19:50:55 -0300

    animation radar

--- app/(root)/missions/page.tsx
+++ app/(root)/missions/page.tsx
@@ -5,17 +5,6 @@ import { motion } from 'framer-motion';
 const Page: React.FC = () => {
   const [isSpinning, setIsSpinning] = useState(false);
 
-  // Asegúrate de que los valores cumplan con los tipos esperados por Framer Motion.
-  const spinAnimation = {
-    rotateY: [0, 360],
-    transition: {
-      duration: 2,
-      ease: "easeOut",
-      times: [0, 1],
-      repeat: 0, // 0 para no repetir, ajusta según la necesidad
-      repeatType: "loop" as const, // Usar 'as const' para asegurar que el tipo sea literalmente "loop"
-    }
-  };
 
   const handleClick = (): void => {
     setIsSpinning(!isSpinning);
@@ -24,19 +13,39 @@ const Page: React.FC = () => {
   return (
     <div className='flex items-center justify-center h-screen'>
         <motion.img 
-            src="https://app-valdi.s3.amazonaws.com/xplorers/isotipo_3+copia.png"
+            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula1.png"
             width={300}
             height={300}
             alt="Logo Xplorers"
-            className="cursor-pointer"
+            className="cursor-pointer absolute"
             whileTap={{ scale: 0.9 }} // Opcional: Efecto de feedback al hacer clic
+            animate={{ rotate: 360 }}
+            transition={{
+              type: "spring",
+              stiffness: 40,
+              damping: 15,
+              mass: 2,
+              restDelta: 0.001, // Ajusta este valor para controlar cuándo la animación debe detenerse
+              from: 0,
+              repeat: 0,
+              duration: 2
+            }}
+            onClick={handleClick}
+        />
+        <motion.img 
+            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula2.png"
+            width={300}
+            height={300}
+            alt="Logo Xplorers"
+            className="cursor-pointer absolute"
+            whileTap={{ rotate:360 }} // Opcional: Efecto de feedback al hacer clic
             animate={{ rotate: 360 }}
             transition={{
               type: "spring",
               stiffness: 60,
               damping: 10,
               mass: 2,
-              restDelta: 0.001, // Ajusta este valor para controlar cuándo la animación debe detenerse
+              restDelta: 0.002, // Ajusta este valor para controlar cuándo la animación debe detenerse
               from: 0,
               repeat: 0,
               duration: 2
--- app/(root)/tokens/page.tsx
+++ app/(root)/tokens/page.tsx
@@ -1,18 +1,59 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
-const page = () => {
+const Page: React.FC = () => {
+  const [isSpinning, setIsSpinning] = useState(false);
+
+
+  const handleClick = (): void => {
+    setIsSpinning(!isSpinning);
+  };
+
   return (
-    <div className='flex items-center justify-center flex-col h-[100dvh]'>
-        <img 
-            src={"https://app-valdi.s3.amazonaws.com/xplorers/isotipo_3+copia.png"}
+    <div className='flex items-center justify-center h-screen'>
+        <motion.img 
+            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula1.png"
             width={300}
             height={300}
-            alt='logo exporers'
-            className='-translate-y-14 spin-y'
-         />
-         {/* <p className='-translate-y-16 text-center px-10  text-white text-[12px] font-bold'>EN <span className='text-[#e6ab45]'>CAMINO</span></p> */}
+            alt="Logo Xplorers"
+            className="cursor-pointer absolute"
+            whileTap={{ scale: 0.9 }} // Opcional: Efecto de feedback al hacer clic
+            animate={{ rotate: 360 }}
+            transition={{
+              type: "spring",
+              stiffness: 40,
+              damping: 15,
+              mass: 2,
+              restDelta: 0.001, // Ajusta este valor para controlar cuándo la animación debe detenerse
+              from: 0,
+              repeat: 0,
+              duration: 2
+            }}
+            onClick={handleClick}
+        />
+        <motion.img 
+            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula2.png"
+            width={300}
+            height={300}
+            alt="Logo Xplorers"
+            className="cursor-pointer absolute"
+            whileTap={{ rotate:360 }} // Opcional: Efecto de feedback al hacer clic
+            animate={{ rotate: 360 }}
+            transition={{
+              type: "spring",
+              stiffness: 60,
+              damping: 10,
+              mass: 2,
+              restDelta: 0.002, // Ajusta este valor para controlar cuándo la animación debe detenerse
+              from: 0,
+              repeat: 0,
+              duration: 2
+            }}
+            onClick={handleClick}
+        />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
