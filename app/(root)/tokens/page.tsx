d92357aeca8b26fb31bac90ca35eec38be429a84
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-07 19:55:42 -0300

    optimizacion de brujula en un solo componente

--- app/(root)/tokens/page.tsx
+++ app/(root)/tokens/page.tsx
@@ -1,59 +1,10 @@
-'use client'
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import Brujula from '@/components/shared/Brujula'
+import React from 'react'
 
-const Page: React.FC = () => {
-  const [isSpinning, setIsSpinning] = useState(false);
-
-
-  const handleClick = (): void => {
-    setIsSpinning(!isSpinning);
-  };
-
+const page = () => {
   return (
-    <div className='flex items-center justify-center h-screen'>
-        <motion.img 
-            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula1.png"
-            width={300}
-            height={300}
-            alt="Logo Xplorers"
-            className="cursor-pointer absolute"
-            whileTap={{ scale: 0.9 }} // Opcional: Efecto de feedback al hacer clic
-            animate={{ rotate: 360 }}
-            transition={{
-              type: "spring",
-              stiffness: 40,
-              damping: 15,
-              mass: 2,
-              restDelta: 0.001, // Ajusta este valor para controlar cuándo la animación debe detenerse
-              from: 0,
-              repeat: 0,
-              duration: 2
-            }}
-            onClick={handleClick}
-        />
-        <motion.img 
-            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula2.png"
-            width={300}
-            height={300}
-            alt="Logo Xplorers"
-            className="cursor-pointer absolute"
-            whileTap={{ rotate:360 }} // Opcional: Efecto de feedback al hacer clic
-            animate={{ rotate: 360 }}
-            transition={{
-              type: "spring",
-              stiffness: 60,
-              damping: 10,
-              mass: 2,
-              restDelta: 0.002, // Ajusta este valor para controlar cuándo la animación debe detenerse
-              from: 0,
-              repeat: 0,
-              duration: 2
-            }}
-            onClick={handleClick}
-        />
-    </div>
-  );
+    <Brujula />
+  )
 }
 
-export default Page;
+export default page
---
+++ components/shared/Brujula.tsx
@@ -0,0 +1,59 @@
+'use client'
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+const Brujula: React.FC = () => {
+  const [isSpinning, setIsSpinning] = useState(false);
+
+
+  const handleClick = (): void => {
+    setIsSpinning(!isSpinning);
+  };
+
+  return (
+    <div className='flex items-center justify-center h-[80dvh]'>
+        <motion.img 
+            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula1.png"
+            width={250}
+            height={250}
+            alt="Logo Xplorers"
+            className="cursor-pointer absolute"
+            whileTap={{ scale: 0.9 }} // Opcional: Efecto de feedback al hacer clic
+            animate={{ rotate: 360 }}
+            transition={{
+              type: "spring",
+              stiffness: 40,
+              damping: 15,
+              mass: 2,
+              restDelta: 0.001, // Ajusta este valor para controlar cuándo la animación debe detenerse
+              from: 0,
+              repeat: 0,
+              duration: 2
+            }}
+            onClick={handleClick}
+        />
+        <motion.img 
+            src="https://app-valdi.s3.amazonaws.com/xplorers/brujula2.png"
+            width={250}
+            height={250}
+            alt="Logo Xplorers"
+            className="cursor-pointer absolute"
+            whileTap={{ rotate:360 }} // Opcional: Efecto de feedback al hacer clic
+            animate={{ rotate: 360 }}
+            transition={{
+              type: "spring",
+              stiffness: 60,
+              damping: 10,
+              mass: 2,
+              restDelta: 0.002, // Ajusta este valor para controlar cuándo la animación debe detenerse
+              from: 0,
+              repeat: 0,
+              duration: 2
+            }}
+            onClick={handleClick}
+        />
+    </div>
+  );
+}
+
+export default Brujula;
