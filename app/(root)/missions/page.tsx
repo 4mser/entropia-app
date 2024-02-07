5e639d0ac27455e542f1aae9ecb6a660d76df000
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-07 19:24:17 -0300

    giro framer motion

--- app/(root)/missions/page.tsx
+++ app/(root)/missions/page.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
-const page = () => {
+const Page: React.FC = () => {
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  // Asegúrate de que los valores cumplan con los tipos esperados por Framer Motion.
+  const spinAnimation = {
+    rotateY: [0, 360],
+    transition: {
+      duration: 2,
+      ease: "easeOut",
+      times: [0, 1],
+      repeat: 0, // 0 para no repetir, ajusta según la necesidad
+      repeatType: "loop" as const, // Usar 'as const' para asegurar que el tipo sea literalmente "loop"
+    }
+  };
+
+  const handleClick = (): void => {
+    setIsSpinning(!isSpinning);
+  };
+
   return (
-    <div className='flex items-center justify-center flex-col h-[100dvh]'>
-        <img 
-            src={"https://app-valdi.s3.amazonaws.com/xplorers/isotipo_3+copia.png"}
+    <div className='flex items-center justify-center h-screen'>
+        <motion.img 
+            src="https://app-valdi.s3.amazonaws.com/xplorers/isotipo_3+copia.png"
             width={300}
             height={300}
-            alt='logo exporers'
-            className='-translate-y-14 spin-y'
-         />
+            alt="Logo Xplorers"
+            className="-translate-y-14 cursor-pointer"
+            animate={isSpinning ? spinAnimation : {}}
+            onClick={handleClick}
+        />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
