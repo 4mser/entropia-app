489bdc88f7fc5e969b74d2ad60b6368ec334d4fa
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-07 22:27:09 -0300

    shadow

--- components/shared/Brujula.tsx
+++ components/shared/Brujula.tsx
@@ -1,8 +1,9 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Brujula: React.FC = () => {
   const [rotation, setRotation] = useState(0);
+  const brujulaRef = useRef(null); // Referencia al div contenedor para obtener su posición
 
   useEffect(() => {
     const handleOrientation = (event: DeviceOrientationEvent) => {
@@ -12,25 +13,43 @@ const Brujula: React.FC = () => {
       }
     };
 
-    window.addEventListener('deviceorientation', handleOrientation, true);
+    const handleMouseMove = (event: MouseEvent) => {
+      if (brujulaRef.current) {
+        const brujula = brujulaRef.current.getBoundingClientRect();
+        const centerX = brujula.left + brujula.width / 2;
+        const centerY = brujula.top + brujula.height / 2;
+        const angle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
+        setRotation(angle);
+      }
+    };
 
+    // Agregar el event listener para el evento 'deviceorientation'
+    window.addEventListener('deviceorientation', handleOrientation, true);
+    // Agregar el event listener para el movimiento del ratón
+    document.addEventListener('mousemove', handleMouseMove);
+
+    // Limpiar los event listeners cuando el componente se desmonte
     return () => {
       window.removeEventListener('deviceorientation', handleOrientation, true);
+      document.removeEventListener('mousemove', handleMouseMove);
     };
   }, []);
 
-  // Clase condicional para aplicar el box shadow. Ajusta los valores según necesites.
+  // Clase condicional para aplicar el box shadow cuando rotation es igual a -13
   const boxShadowClass = rotation === -45 ? 'shadow-lg ring-1 ring-amber-500/50 filter drop-shadow-lg' : 'filter';
 
+  // Verifica si rotation es igual a -45 o 115 para mostrar la etiqueta "N"
+  const showN = rotation === -45 || rotation === 115;
+
   return (
-    <div className='flex items-center justify-center h-[80vh]'>
-        <p className='text-[#DD981D] absolute text-[15px]'>N</p>
+    <div ref={brujulaRef} className='flex items-center justify-center h-[80vh]'>
+        {showN && <p className='text-[#DD981D] absolute text-[25px] font-bold top-28'>N</p>}
         <img 
             src="https://app-valdi.s3.amazonaws.com/xplorers/brujula1.png"
             width={250}
             height={250}
             alt="Base de la brújula"
-            className={`cursor-pointer ${rotation === -45 ? 'filter drop-shadow-md shadow-amber-400' : ''}`}
+            className={`cursor-pointer ${rotation === -45 ? 'shadow-lg shadow-amber-500/50' : ''}`}
             style={{ position: 'absolute' }}
         />
         <img 
