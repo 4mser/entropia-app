ffbab34ddfac8f714150fea9d53e35cd36e4b4ab
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-07 21:39:40 -0300

    test brujula

--- components/shared/Brujula.tsx
+++ components/shared/Brujula.tsx
@@ -1,71 +1,43 @@
 'use client'
-
 import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
 
 const Brujula: React.FC = () => {
-  const [rotation, setRotation] = useState<number>(0);
+  const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
     const handleOrientation = (event: DeviceOrientationEvent) => {
       const { alpha } = event;
-      setRotation(alpha || 0);
-    };
-
-    const requestOrientationPermission = async () => {
-      // Verifica si DeviceOrientationEvent.requestPermission está disponible
-      if ('DeviceOrientationEvent' in window && typeof (DeviceOrientationEvent as any).requestPermission === 'function') {
-        try {
-          const permissionState = await (DeviceOrientationEvent as any).requestPermission();
-          if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', handleOrientation);
-          } else {
-            console.error("Permission not granted for DeviceOrientation");
-          }
-        } catch (error) {
-          console.error("Error requesting device orientation permission:", error);
-        }
-      } else {
-        // requestPermission no es necesario o no está disponible, agrega el listener directamente
-        window.addEventListener('deviceorientation', handleOrientation);
+      if (alpha !== null) {
+        setRotation(alpha);
       }
     };
 
-    // Solicita permiso al cargar el componente
-    requestOrientationPermission();
+    // Agregar el event listener para el evento 'deviceorientation'
+    window.addEventListener('deviceorientation', handleOrientation, true);
 
-    // Limpieza al desmontar el componente
+    // Limpiar el event listener cuando el componente se desmonte
     return () => {
-      window.removeEventListener('deviceorientation', handleOrientation);
+      window.removeEventListener('deviceorientation', handleOrientation, true);
     };
   }, []);
 
   return (
     <div className='flex items-center justify-center h-[80vh]'>
-        <motion.img 
+        <img 
             src="https://app-valdi.s3.amazonaws.com/xplorers/brujula1.png"
             width={250}
             height={250}
-            alt="Logo Xplorers"
-            className="cursor-pointer absolute"
+            alt="Base de la brújula"
+            className="cursor-pointer"
+            style={{ position: 'absolute' }}
         />
-        <motion.img 
+        <img 
             src="https://app-valdi.s3.amazonaws.com/xplorers/brujula2.png"
             width={250}
             height={250}
-            alt="Logo Xplorers"
-            className="cursor-pointer absolute"
-            style={{ rotate: rotation }}
-            transition={{
-              type: "spring",
-              stiffness: 60,
-              damping: 10,
-              mass: 2,
-              restDelta: 0.002,
-              from: 0,
-              repeat: 0,
-              duration: 2
-            }}
+            alt="Aguja de la brújula"
+            className="cursor-pointer"
+            style={{ transform: `rotate(${rotation}deg)`, position: 'absolute' }}
         />
     </div>
   );
