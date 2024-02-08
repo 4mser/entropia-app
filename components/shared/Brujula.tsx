d63f5b18e85f4cea8e42b57a33a55399d0c95a1d
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-07 22:14:53 -0300

    deg

--- components/shared/Brujula.tsx
+++ components/shared/Brujula.tsx
@@ -12,24 +12,25 @@ const Brujula: React.FC = () => {
       }
     };
 
-    // Agregar el event listener para el evento 'deviceorientation'
     window.addEventListener('deviceorientation', handleOrientation, true);
 
-    // Limpiar el event listener cuando el componente se desmonte
     return () => {
       window.removeEventListener('deviceorientation', handleOrientation, true);
     };
   }, []);
 
+  // Clase condicional para aplicar el box shadow. Ajusta los valores según necesites.
+  const boxShadowClass = rotation === -45 ? 'shadow-lg ring-1 ring-amber-500/50 filter drop-shadow-lg' : 'filter';
+
   return (
     <div className='flex items-center justify-center h-[80vh]'>
-        <p className='text-[DD981D] absolute text-[15px]'>N</p>
+        <p className='text-[#DD981D] absolute text-[15px]'>N</p>
         <img 
             src="https://app-valdi.s3.amazonaws.com/xplorers/brujula1.png"
             width={250}
             height={250}
             alt="Base de la brújula"
-            className="cursor-pointer"
+            className={`cursor-pointer ${rotation === -45 ? 'shadow-amber-500/50' : ''}`}
             style={{ position: 'absolute' }}
         />
         <img 
@@ -37,7 +38,7 @@ const Brujula: React.FC = () => {
             width={250}
             height={250}
             alt="Aguja de la brújula"
-            className="cursor-pointer filter drop-shadow-[#dd981d]"
+            className={`cursor-pointer ${boxShadowClass}`}
             style={{ transform: `rotate(${rotation}deg)`, position: 'absolute' }}
         />
     </div>
